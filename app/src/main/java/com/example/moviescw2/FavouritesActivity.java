e8a28c035f268244c1dc8c42256e57fd70524fe4
ConalTyre <51913316+ConalTyre@users.noreply.github.com>
2021-11-18 16:19:36 +0000

    Activities

---
+++ app/src/main/java/com/example/moviescw2/FavouritesActivity.java
@@ -0,0 +1,14 @@
+package com.example.moviescw2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FavouritesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourites);
+    }
+}
